cf9e03d046e03f1ae8be5bfcb19a36e7f01e7fb7
brickyang <brick.c.yang@gmail.com>
2018-08-28 10:06:08 +0800

    feat: crop slim photo

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -127,10 +127,7 @@ function loadImage() {
 }
 
 function cropImage(image) {
-  const width = image.width;
-  const height = Math.round(width * TARGET_RATIO);
-  const cropX = 0;
-  const cropY = Math.round((image.height - height) / 2);
+  const { width, height, cropX, cropY } = getCropArgs(image);
 
   const canvas = document.createElement('canvas');
   canvas.width = width;
@@ -140,4 +137,20 @@ function cropImage(image) {
   ctx.drawImage(image, cropX, cropY, width, height, 0, 0, width, height);
 
   return canvas.toDataURL();
+}
+
+function getCropArgs(image) {
+  let width = image.width;
+  let height = Math.round(width * TARGET_RATIO);
+  let cropX = 0;
+  let cropY = Math.round((image.height - height) / 2);
+
+  if (image.height < height) {
+    height = image.height;
+    width = Math.round(height / TARGET_RATIO);
+    cropX = Math.round((image.width - width) / 2);
+    cropY = 0;
+  }
+
+  return { width, height, cropX, cropY };
 }
